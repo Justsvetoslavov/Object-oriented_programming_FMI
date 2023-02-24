7ebca17f18fb693c723126cba9be9105e6499fa5
KirilGeorgiev247 <kiril.georgiev2003@gmail.com>
2023-02-24 16:42:52 +0200

    Task-04
    Solution to task implemented

---
+++ Seminars/Sem.01/Pract.01/Task04.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+void copyStr(char* copy, const char* arr)
+{
+	int count = 0;
+
+	while (arr[count] != '\0')
+	{
+		copy[count] = arr[count];
+		count++;
+	}
+
+	copy[count] = '\0';
+}
+
+int getStrLength(const char* str)
+{
+	int count = 0;
+
+	while (str[count++] != '\0')
+	{
+
+	}
+
+	return count;
+}
+
+struct Student
+{
+	unsigned int facultyNumber;
+	char name[MAX_NAME_SIZE];
+};
+
+void initStudent(Student& student, const char* name, const unsigned int& facultyNumber)
+{
+	if (getStrLength(name) <= 30)
+	{
+		copyStr(student.name, name);
+		student.facultyNumber = facultyNumber;
+	}
+}
+
+void printStudentInfo(const Student& student)
+{
+	const char* studentName = student.name;
+
+	cout << "Student:" << endl << "Name: " << studentName << endl << "FN: " << student.facultyNumber;
+}
+
+int main()
+{
+	Student st = { "Gosho", 10 }; // can be predefined, properties can be changed
+	Student* pSt = &st; // can be predefined, properties can be changed
+	const Student* pCSt = &st; // can be predefined, properties cannot be changed
+	Student* const cPSt = &st; // cannot be predefined, properties can be changed
+	const Student* const cpCSt = &st; // cannot be predefined, properties cannot be changed
+	Student& refSt = st; // can be predefined, properties can be changed
+	const Student& refCSt = st; // cannot be predefined, properties cannot be changed
+
+	Student student;
+	initStudent(student, "Pesho", 20);
+	printStudentInfo(student);
+}

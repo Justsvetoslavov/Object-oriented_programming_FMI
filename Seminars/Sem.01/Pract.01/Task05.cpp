5e25fb01af4e772895ce7484b6a064ac91b08641
raiagrigorova <raiagrigorova@gmail.com>
2023-02-25 14:40:53 +0200

    Create Task05.cpp

---
+++ Seminars/Sem.01/Pract.01/Task05.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+
+struct ComplexNumber {
+	double realPart;
+	double complexPart;
+};
+
+ComplexNumber addComplexNumbers(const ComplexNumber& a, const ComplexNumber& b){
+	ComplexNumber c;
+
+	//Add the real parts and the imaginary parts together
+	c.realPart = a.realPart + b.realPart;
+	c.complexPart = a.complexPart + b.complexPart;
+
+	return c;
+}
+
+void printComplexNumber(const ComplexNumber& a) {
+	std::cout << a.realPart << " " << a.complexPart << "\n";
+}
+
+int main()
+{
+	ComplexNumber cn1 = { 3, 4 };
+	ComplexNumber cn2 = { -10, 12 };
+	ComplexNumber sum = addComplexNumbers(cn1, cn2);
+	printComplexNumber(sum);
+}
+

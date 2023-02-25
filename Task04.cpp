ca9148a2799750b66b52a8f7935068d4dadea061
N1k1ta-white <65460812+N1k1ta-white@users.noreply.github.com>
2023-02-26 01:06:13 +0200

    Task4

---
+++ Task04.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+struct Student {
+    char name[30];
+    uint64_t fac_num;
+};
+
+void initStudent(const Student& student) {
+    cout << "Name: " << student.name << endl
+         << "Fac. number: " << student.fac_num << endl;
+}
+
+void str_trans(char str1[], const char str2[]) {
+    int rotator = -1;
+    while (str2[++rotator] != '\0')
+        str1[rotator] = str2[rotator];
+}
+
+void change_info(Student& student, const char name[], int fac_num) {
+    str_trans(student.name, name);
+    student.fac_num = fac_num;
+}
+
+int main() {
+    Student st = { "Gosho", 10 };
+    initStudent(st);
+    Student* pSt = &st;
+    initStudent(*pSt);
+    change_info(*pSt, "Test1", 1);
+    initStudent(*pSt);
+    const Student* pCSt = &st;
+    initStudent(*pCSt);
+//    change_info(*pCSt, "Test2", 2);  can't change
+//    initStudent(*pCSt);
+    Student* const cPSt = &st;
+    initStudent(*cPSt);
+    change_info(*cPSt, "Test3", 3);
+    initStudent(*cPSt);
+    const Student* const cpCSt = &st;
+    initStudent(*cpCSt);
+//    change_info(*cpCSt, "Test4", 4);  can't change
+//    initStudent(*cpCSt);
+    Student& refSt = st;
+    initStudent(refSt);
+    change_info(refSt, "Test5", 5);
+    initStudent(refSt);
+    const Student& refCSt = st;
+    initStudent(refCSt);
+//    change_info(refCSt, "Test6", 6);  can't change
+//    initStudent(refCSt);
+}

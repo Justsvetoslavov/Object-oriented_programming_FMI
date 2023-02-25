1a108bdc10c1490bc453c08d343f345afb4b2dc5
N1k1ta-white <65460812+N1k1ta-white@users.noreply.github.com>
2023-02-26 01:11:52 +0200

    Task5

---
+++ Task05.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+struct Complex_num {
+    int r, i;
+};
+
+Complex_num sum_complex_num(const Complex_num &a, const Complex_num &b) {
+    Complex_num res{ a.r + b.r, a.i + b.i };
+    return res;
+}
+
+void read_complex_num(const Complex_num &num) {
+    cout << num.r << " + " << num.i << "i\n";
+}
+
+int main() {
+    Complex_num n1{3, 5}, n2{4, 3};
+    read_complex_num(n1);
+    Complex_num sum = sum_complex_num(n1, n2);
+    read_complex_num(sum);
+}

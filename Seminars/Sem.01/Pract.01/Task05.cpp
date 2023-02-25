4158bce9906e86893c4210eeef486b2548c402c2
Sevda18 <sevda182003@gmail.com>
2023-02-25 18:18:20 +0200

    Create Task06.cpp

---
+++ Seminars/Sem.01/Pract.01/Task05.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+
+using namespace std;
+
+struct ComplexNumber
+{
+    double real;
+    double imaginary;
+};
+
+void inputNumber(ComplexNumber& num)
+{
+    cout << "Enter the real part of the number: ";
+    cin >> num.real;
+    cout << "Enter the imaginary part of the number: ";
+    cin >> num.imaginary;
+};
+
+ComplexNumber sum(const ComplexNumber& num1, const ComplexNumber& num2) {
+    double realSum = num1.real + num2.real;
+    double imagSum = num1.imaginary + num2.imaginary;
+    return { realSum, imagSum };
+}
+
+void printComplexNumber(const ComplexNumber& num)
+{
+    if (num.imaginary >= 0) {
+        cout << num.real << " + " << num.imaginary << "i" << endl;
+    }
+    else {
+        cout << num.real << " - " << num.imaginary << "i" << endl;
+    }
+}
+
+int main()
+{
+    ComplexNumber(num1);
+    inputNumber(num1);
+    printComplexNumber(num1);
+    cout << endl;
+
+    ComplexNumber(num2);
+    inputNumber(num2);
+    printComplexNumber(num2);
+    cout << endl;
+
+    cout << "The sum of the complex numbers is: ";
+    ComplexNumber result = sum(num1, num2);
+    printComplexNumber(result);
+
+    return 0;
+}

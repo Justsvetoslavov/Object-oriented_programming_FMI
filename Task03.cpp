0c1f7f4860e63bd86c5d37300d47f3148b4e1cc4
N1k1ta-white <65460812+N1k1ta-white@users.noreply.github.com>
2023-02-27 00:11:09 +0200

    Update Task03.cpp

--- Task03.cpp
+++ Task03.cpp
@@ -4,38 +4,38 @@ using std::cout;
 using std::cin;
 using std::endl;
 
-int** create_matrix(int size1, int size2) {
-    int** matrix = new int*[size1];
-    for (size_t i = 0; i < size1; i++)
-        matrix[i] = new int[size2];
+int** create_matrix(int row, int col) {
+    int** matrix = new int*[row];
+    for (size_t i = 0; i < row; i++)
+        matrix[i] = new int[col];
     return matrix;
 }
 
-void fill_matrix(int** matrix, int size1, int size2) {
-    for (size_t i = 0; i < size1; i++)
-        for (size_t j = 0; j < size2; j++)
-            matrix[i][j] = i * size2 + j + 1;
+void fill_matrix(int** matrix, int row, int col) {
+    for (int i = 0; i < row; ++i)
+        for (int j = 0; j < col; ++j)
+            cin >> matrix[i][j];
 }
 
-int** transpon_matrix(int** matrix, int size1, int size2) {
-    int** tran_matrix = create_matrix(size2, size1);
-    for (size_t i = 0; i < size1; i++)
-        for (size_t j = 0; j < size2; j++)
+int** transpon_matrix(const int* const* matrix, int row, int col) {
+    int** tran_matrix = create_matrix(col, row);
+    for (size_t i = 0; i < row; i++)
+        for (size_t j = 0; j < col; j++)
             tran_matrix[j][i] = matrix[i][j];
     return tran_matrix;
 }
 
-void delete_matrix(int** matrix, int size) {
-    for (size_t i = 0; i < size; i++)
+void delete_matrix(int** matrix, int row) {
+    for (size_t i = 0; i < row; i++)
     {
         delete[] matrix[i];
     }
     delete[] matrix;
 }
 
-void read_matrix(int** matrix, int size1, int size2) {
-    for (size_t i = 0; i < size1; i++) {
-        for (size_t j = 0; j < size2; j++)
+void read_matrix(const int* const* matrix, int row, int col) {
+    for (size_t i = 0; i < row; i++) {
+        for (size_t j = 0; j < col; j++)
             cout << matrix[i][j] << " ";
         cout << endl;
     }
@@ -44,8 +44,8 @@ void read_matrix(int** matrix, int size1, int size2) {
 int main() {
     int n, m;
     cin >> n >> m;
-    int** matrix = create_matrix(n, m);
-    fill_matrix(matrix, n, m);
+    int **matrix = create_matrix(n, m);
+    fill_matrix(matrix, n ,m);
     read_matrix(matrix, n, m);
     matrix = transpon_matrix(matrix, n, m);
     read_matrix(matrix, m, n);

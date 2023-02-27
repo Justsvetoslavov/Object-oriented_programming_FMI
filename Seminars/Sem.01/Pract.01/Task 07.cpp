fdafede58f602e3de5f0f5160543ebc7d2d1799c
Natalia N <nakollofskinatalia@gmail.com>
2023-02-28 00:53:58 +0200

    Solutions to tasks 7 and 8

---
+++ Seminars/Sem.01/Pract.01/Task 07.cpp
@@ -0,0 +1,96 @@
+﻿#include <iostream>
+using std::cin;
+using std::cout;
+using std::endl;
+//Natalia Nakollofski   1MI8000034    GR.1
+
+//Реализирайте структура, описваща филм(Movie) със следните характеристики :
+//
+//Име на филм
+//Продължителност в минути
+//Жанр
+//Възможните стойности са описани с множеството{ Comedy, Action, Horror, Romantic }
+//Рейтинг
+//Създайте масив от филми и имплементирайте следната функционалност :
+//
+//Функция, която създава филм по подадени параметри.
+//Spoiler : По принцип тази задача се делегира на конструкторите, които ще изучаваме скоро.
+//
+//Направете масив от филми и добавете 3 филма по ваш избор.
+//Изведете всички филми в масива.
+//Изведете топ филма в масива - този с най - много рейтинг.
+//Бонус :
+//
+//	Сортиране на филмите по рейтинг.
+
+
+
+struct Movie {
+	char name[500];
+	int  duration;
+	char genre[120];
+	double rating;
+};
+
+
+void topmovie(Movie& mymov1, Movie& mymov2, Movie& mymov3) {
+	cout << "TOP 3 MOVIES :";
+	if (mymov1.rating >= mymov2.rating && mymov1.rating >= mymov3.rating) {
+		
+		cout << "MOVIE NAME:" << mymov1.name << ' ' << "DURATION:" << mymov1.duration << ' '<<"min"<<endl << "GENRE:" << mymov1.genre << endl << "RATING:" << mymov1.rating << endl;
+		
+		if (mymov2.rating >= mymov3.rating) {
+			cout << "MOVIE NAME:" << mymov2.name << ' ' << "DURATION:" << mymov2.duration << '  '<<"min" << endl << "GENRE:" << mymov2.genre << endl << "RATING:" << mymov2.rating << endl;
+			cout << endl;
+			cout << "MOVIE NAME:" << mymov3.name << ' ' << "DURATION:" << mymov3.duration << '  '<<"min"<<endl << "GENRE:" << mymov3.genre << endl << "RATING:" << mymov3.rating << endl;
+		}
+		else {
+			cout << "MOVIE NAME:" << mymov3.name << ' ' << "DURATION:" << mymov3.duration << '  ' << "min" << endl << "GENRE:" << mymov3.genre << endl << "RATING:" << mymov3.rating << endl;
+
+			cout << "MOVIE NAME:" << mymov2.name << ' ' << "DURATION:" << mymov2.duration << '  ' << endl << "GENRE:" << mymov2.genre << endl << "RATING:" << mymov2.rating << endl;
+		}
+	}
+	else if (mymov2.rating >= mymov1.rating && mymov2.rating >= mymov3.rating) {
+
+		cout << "MOVIE NAME:" << mymov2.name << ' ' << "DURATION:" << mymov2.duration << '  '<<"min" << endl << "GENRE:" << mymov2.genre << endl << "RATING:" << mymov2.rating << endl;
+
+		if (mymov1.rating >= mymov3.rating) {
+			cout << "MOVIE NAME:" << mymov1.name << ' ' << "DURATION:" << mymov1.duration << ' ' << "min" << endl << "GENRE:" << mymov1.genre << endl << "RATING:" << mymov1.rating << endl;
+
+			cout << "MOVIE NAME:" << mymov3.name << ' ' << "DURATION:" << mymov3.duration << '  ' << "min" << endl << "GENRE:" << mymov3.genre << endl << "RATING:" << mymov3.rating << endl;
+		}
+		else {
+			cout << "MOVIE NAME:" << mymov3.name << ' ' << "DURATION:" << mymov3.duration << '  ' << "min" << endl << "GENRE:" << mymov3.genre << endl << "RATING:" << mymov3.rating << endl;
+
+			cout << "MOVIE NAME:" << mymov1.name << ' ' << "DURATION:" << mymov1.duration << ' ' << "min" << endl << "GENRE:" << mymov1.genre << endl << "RATING:" << mymov1.rating << endl;
+
+		}
+	}
+	else if (mymov3.rating >= mymov1.rating && mymov3.rating >= mymov2.rating) {
+
+		cout << "MOVIE NAME:" << mymov3.name << ' ' << "DURATION:" << mymov3.duration << '  ' << "min" << endl << "GENRE:" << mymov3.genre << endl << "RATING:" << mymov3.rating << endl;
+
+		if (mymov1.rating >= mymov2.rating) {
+			cout << "MOVIE NAME:" << mymov1.name << ' ' << "DURATION:" << mymov1.duration << ' ' << "min" << endl << "GENRE:" << mymov1.genre << endl << "RATING:" << mymov1.rating << endl;
+			cout << "MOVIE NAME:" << mymov2.name << ' ' << "DURATION:" << mymov2.duration << '  ' <<"min" << endl << "GENRE:" << mymov2.genre << endl << "RATING:" << mymov2.rating << endl;
+		}
+		else {
+
+			cout << "MOVIE NAME:" << mymov2.name << ' ' << "DURATION:" << mymov2.duration << '  '<<"min" << endl << "GENRE:" << mymov2.genre << endl << "RATING:" << mymov2.rating << endl;
+			cout << "MOVIE NAME:" << mymov1.name << ' ' << "DURATION:" << mymov1.duration << ' ' << "min" << endl << "GENRE:" << mymov1.genre << endl << "RATING:" << mymov1.rating << endl;
+
+		}
+
+	}
+
+}
+
+
+
+int main() {
+	Movie mymovie1={"The lost city",120,"Adventure",5.5};
+	Movie mymovie2 = { "Spider-man",184,"Science-fiction",5.0 };
+	Movie mymovie3 = { "Avatar",193,"Science-fiction",6.0 };
+	topmovie(mymovie1, mymovie2, mymovie3);
+
+}
---
+++ Seminars/Sem.01/Pract.01/Task 08.cpp
@@ -0,0 +1,93 @@
+﻿#include <iostream>
+using std::cin;
+using std::cout;
+using std::endl;
+//Natalia Nakollofski   1MI8000034    GR.1
+
+
+//Една група се състои от краен брой студенти, като броят на студентите се задава от вас.
+//
+//Създайте структура, която описва група.Структурата също така трябва да пази средния успех на групата.
+//
+//Направете функция, която създава група, вземайки информация от стандартния вход.Средния успех трябва да се сметне след въвеждане на информацията.
+//Напишете функция, която приема група и минимален успех за стипендия, и връща колко студента ще получават стипендия.
+//Създайте функция, която приема група и минимален успех за стипендия и извежда сортирани всички студенти, които ще получават стипендия
+//идния семестър.
+
+const int MAX = 50;
+struct Student {
+	char name[50];
+	double grade;
+};
+struct Group {
+	int numberofstudents;
+	double gpa;
+};
+
+void inputdata(Student mystudent[], Group& mygroup) {
+	cout << "enter nr of students in the group:";
+	cin >> mygroup.numberofstudents;
+	mygroup.gpa = 0;
+	
+	for (int i = 0; i < mygroup.numberofstudents; i++) {
+		cin >> mystudent[i].name >> mystudent[i] .grade;
+		mygroup.gpa += mystudent[i].grade;
+	}
+	mygroup.gpa /= mygroup.numberofstudents;
+
+
+}
+void  readinput(Student mystudent[], Group& mygroup) {
+	for (int i = 0; i < mygroup.numberofstudents; i++) {
+		cout << mystudent[i].name <<' '<< endl << mystudent[i].grade<<' '<<endl;
+		
+	}
+	cout << "GROUP GPA IS:";
+	cout << mygroup.gpa << endl;
+
+}
+void provideScholarship(Student mystud[], Group& mygroup) {
+	double minimumScholarship = 5.5;
+	int num = 0;
+	
+	for (int i = 0; i < mygroup.numberofstudents; i++) {
+		if (mystud[i].grade >= minimumScholarship) {
+			num++;
+		}
+
+	}
+	cout << "Number of student who will gain a scholarship is:" << num << endl;
+
+
+	cout << "STUDETNS WHO WILL GAIN A SCHOLARSHIP BASED ON THEIR GPA ARE:" << endl;
+	for (int i = 0; i < mygroup.numberofstudents; i++) {
+		for(int j=0;j<num;j++){
+		if (mystud[j].grade <= mystud[j+1].grade) {
+			Student temp = mystud[j];
+			mystud[j] = mystud[j + 1];
+			mystud[j + 1] = temp;
+				
+			}
+		}
+
+	}
+	for (int i = 0; i < mygroup.numberofstudents; i++) {
+		if (mystud[i].grade >= minimumScholarship) {
+			cout << mystud[i].name << ' ' << mystud[i].grade;
+		}
+	}
+
+
+
+}
+
+
+int main() {
+	cout << "Enter datas for students" << endl;
+	Student mystudent[MAX];
+	Group mygroup;
+	inputdata(mystudent, mygroup);
+	cout << "STUDENTS AND THEIR GRADES ARE:" << endl;
+	readinput(mystudent, mygroup);
+	provideScholarship(mystudent, mygroup);
+}

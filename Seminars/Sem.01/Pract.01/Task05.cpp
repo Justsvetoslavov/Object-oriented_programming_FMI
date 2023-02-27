e6447e3449ee0a6ec3f8a68a7c5c548895652def
ivan9643 <88584871+ivan9643@users.noreply.github.com>
2023-02-27 17:03:52 +0200

    Create Task05.cpp

---
+++ Seminars/Sem.01/Pract.01/Task05.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+
+struct ComplexNumber {
+	double realPart;
+	double imaginaryPart;
+};
+
+ComplexNumber Add(ComplexNumber a, ComplexNumber b) {
+	return ComplexNumber{ a.realPart + b.realPart, a.imaginaryPart + b.imaginaryPart };
+}
+
+void PrintComplexNumber(ComplexNumber c) {
+	cout << c.realPart << " + " << c.imaginaryPart << "i";
+}
+
+int main() {
+	ComplexNumber result = Add(ComplexNumber{ 1, 4.4 }, ComplexNumber{ 3.4,5 });
+	PrintComplexNumber(result);
+}

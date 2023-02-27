59583e09c72d0ba58f38ad2b42da19a5edb8ef11
velislavav <115610829+velislavav@users.noreply.github.com>
2023-02-27 17:30:28 +0200

    Add files via upload

--- Seminars/Sem.01/Pract.01/Task01.cpp
+++ Seminars/Sem.01/Pract.01/Task01.cpp
@@ -1,28 +1,27 @@
 #include <iostream>
 
-using std::cin, std::cout, std::endl;
-
-int lowerByPointer(const int *num1, const int *num2) {
-    return *num1 < *num2 ? *num1 : *num2;
+int lowerNumByCopie(const int num1, const int num2)
+{
+	return num1 < num2 ? num1 : num2;
 }
 
-int lowerByReference(const int &num1, const int &num2) {
-    return lowerByPointer(&num1, &num2);
+int lowerNumByReference(const int& num1, const int& num2)
+{
+	return num1 < num2 ? num1 : num2;
 }
 
-int lowerByReference(int &num1, int &num2) {
-    return lowerByPointer(&num1, &num2);
+int lowerNumByPointer(int* num1, int* num2)
+{
+	return *num1 < *num2 ? *num1 : *num2;
 }
 
-int main() {
-    int num1, num2;
-    const int number3 = 3;
-    cout << "Enter two numbers: ";
-    cin >> num1 >> num2;
-    //Invokes the lowerByReference(const int &, const int &) function because the arguments are constant
-    cout << "The lower by ref number is " << lowerByReference(num1, num2) << endl;
-    //Invokes the lowerByReference(int &, int &) function because the arguments are not constant
-    cout << "The lower by ref number is " << lowerByReference(num1, number3) << endl;
-    cout << "The lower by pointer number is " << lowerByPointer(&num1, &num2) << endl;
-    return 0;
+int main()
+{
+	int num1, num2;
+	std::cin >> num1 >> num2;
+
+	std::cout << lowerNumByCopie(num1, num2) << std::endl;
+	std::cout << lowerNumByReference(num1, num2) << std::endl;
+	std::cout << lowerNumByPointer(&num1, &num2) << std::endl;
 }
+	

7a6738c09360b68186981f7d8f5cc70b538605b0
plamenstoynev <79408379+plamenstoynev@users.noreply.github.com>
2023-02-27 15:42:44 +0200

    Update Task03.cpp

--- Seminars/Sem.01/Pract.01/Task03.cpp
+++ Seminars/Sem.01/Pract.01/Task03.cpp
@@ -1,13 +1,10 @@
-#include <iostream>
-//Task 3
-int** createMatrix(size_t rows, size_t cols)
+void createMatrix(int**& matrix, size_t rows, size_t cols)
 {
-    int** matrix = new int*[rows];
+    matrix = new int*[rows];
     for (size_t i = 0; i < rows; i++)
     {
         matrix[i] = new int[cols];
     }
-    return matrix;
 }
 int** inputMatrix(int** matrix, size_t rows, size_t cols)
 {
@@ -59,7 +56,8 @@ int main()
     std::cin >> rows >> cols;
     transCols = rows;
     transRows = cols;
-    int** matrix = createMatrix(rows, cols);
+    int** matrix;
+    createMatrix(matrix, rows, cols);
     matrix = inputMatrix(matrix, rows, cols);
     int** newMatrix = transMatrix(matrix, rows, cols);
     printMatrix(matrix, rows, cols);

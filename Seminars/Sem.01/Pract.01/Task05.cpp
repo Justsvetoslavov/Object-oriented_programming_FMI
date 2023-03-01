36acf39a5240bb14e63bb463ba359c5ec6cff8e9
Yonko Draganov <draganovyonko@gmail.com>
2023-03-01 13:27:11 +0200

    Task05

--- Seminars/Sem.01/Pract.01/Task05.cpp
+++ Seminars/Sem.01/Pract.01/Task05.cpp
@@ -1,24 +1,91 @@
 #include <iostream>
 
-using std::cin, std::cout, std::endl;
-
-struct ComplexNumber {
-    double real;
-    double imaginary;
+struct ComplexNumber
+{
+	double realPart;
+	double imaginaryPart;
 };
 
-ComplexNumber additionOfComplexNumbers(const ComplexNumber &lhs, const ComplexNumber &rhs) {
-    return {lhs.real + rhs.real, lhs.imaginary + rhs.imaginary};
+ComplexNumber sumComplexNumbers(const ComplexNumber& num1, const ComplexNumber& num2)
+{
+	ComplexNumber result{ 0, 0 };
+	result.realPart = num1.realPart + num2.realPart;
+	result.imaginaryPart = num1.imaginaryPart + num2.imaginaryPart;
+	return result;
 }
 
-void printComplexNumber(const ComplexNumber &number) {
-    cout << number.real << " + " << number.imaginary << "i" << endl;
+bool compareDoubles(const double& d1, const double& d2)
+{
+	const double EPSILON = 0.0000001;
+	return abs(d1 - d2) < EPSILON;
 }
 
-int main() {
-    ComplexNumber a = {1.0, 2.0};
-    ComplexNumber b = {3.0, 4.0};
-    ComplexNumber c = additionOfComplexNumbers(a, b);
-    printComplexNumber(c);
-    return 0;
+void printComplexNumber(const ComplexNumber& num)
+{
+	const bool hasRealPart = num.realPart > 0 || num.realPart < 0;
+	const bool hasImaginaryPart = num.imaginaryPart > 0 || num.imaginaryPart < 0;
+	const bool imaginaryPartCloseToOne = compareDoubles(num.imaginaryPart, 1.);
+	const bool imaginaryPartCloseToMinusOne = compareDoubles(num.imaginaryPart, -1.);
+
+	if (hasRealPart)
+	{
+		std::cout << num.realPart;
+		if (hasImaginaryPart)
+		{
+			if (num.imaginaryPart > 0)
+			{
+				std::cout << " + ";
+			}
+			else if (num.imaginaryPart < 0)
+			{
+				std::cout << " - ";
+			}
+
+			if (!imaginaryPartCloseToOne && !imaginaryPartCloseToMinusOne)
+			{
+				std::cout << abs(num.imaginaryPart);
+			}
+			std::cout << 'i';
+		}
+	}
+	else
+	{
+		if (hasImaginaryPart)
+		{
+			if (num.imaginaryPart < 0)
+			{
+				std::cout << " - ";
+			}
+
+			if (!imaginaryPartCloseToOne && !imaginaryPartCloseToMinusOne)
+			{
+				std::cout << num.imaginaryPart;
+			}
+			std::cout << 'i';
+		}
+		else
+		{
+			std::cout << num.realPart << '+' << num.imaginaryPart << 'i';
+		}
+	}
+}
+
+int main()
+{
+	ComplexNumber c1{ 1,2 };
+	ComplexNumber c2{ -2,4 };
+	printComplexNumber(c1);
+	std::cout << '\n';
+	printComplexNumber(c2);
+	std::cout << '\n';
+	printComplexNumber(sumComplexNumbers(c1, c2));
+	std::cout << "\n\n";
+
+	ComplexNumber c3{ 0.0025,-1.0001 };
+	ComplexNumber c4{ -1.0011,0.0001 };
+	printComplexNumber(c3);
+	std::cout << '\n';
+	printComplexNumber(c4);
+	std::cout << '\n';
+	printComplexNumber(sumComplexNumbers(c3, c4));
 }

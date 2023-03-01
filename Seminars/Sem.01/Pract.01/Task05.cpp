498dd56804b39aba0c1c058ed6d84a91cdf3b85d
gabrielavm <115610325+gabrielavm@users.noreply.github.com>
2023-03-02 00:46:19 +0200

    Update Task05.cpp

--- Seminars/Sem.01/Pract.01/Task05.cpp
+++ Seminars/Sem.01/Pract.01/Task05.cpp
@@ -1,24 +1,46 @@
+//Gabriela Mladenova 
+//SI
+//Group 1
+//3MI0600225
 #include <iostream>
 
-using std::cin, std::cout, std::endl;
-
-struct ComplexNumber {
-    double real;
-    double imaginary;
+struct complexNumber
+{
+    int Re;
+    int Im;
 };
-
-ComplexNumber additionOfComplexNumbers(const ComplexNumber &lhs, const ComplexNumber &rhs) {
-    return {lhs.real + rhs.real, lhs.imaginary + rhs.imaginary};
+void inputComplexNumber(complexNumber& number)
+{
+    std::cout << "Enter the real part of the number: ";
+    std::cin >> number.Re;
+    std::cout << "Enter the imaginary part of the number: ";
+    std::cin >> number.Im;
 }
-
-void printComplexNumber(const ComplexNumber &number) {
-    cout << number.real << " + " << number.imaginary << "i" << endl;
+complexNumber sumOfComplexNumbers(const complexNumber number1, const complexNumber number2)
+{
+    complexNumber result;
+    result.Re = number1.Re + number2.Re;
+    result.Im = number1.Im + number2.Im;
+    return result;
 }
+void printComplexNumber(const complexNumber& number)
+{
+    std::cout << number.Re << " + " << number.Im << "i";
+}
+int main()
+{
+    complexNumber number1 = {};
+    complexNumber number2 = {};
+    complexNumber resultOfSum = {};
+    inputComplexNumber(number1);
+    inputComplexNumber(number2);
 
-int main() {
-    ComplexNumber a = {1.0, 2.0};
-    ComplexNumber b = {3.0, 4.0};
-    ComplexNumber c = additionOfComplexNumbers(a, b);
-    printComplexNumber(c);
+    resultOfSum = sumOfComplexNumbers(number1, number2);
+    printComplexNumber(number1);
+    std::cout << "  +  ";
+    printComplexNumber(number2);
+    std::cout << "  =  ";
+    printComplexNumber(resultOfSum);
+
     return 0;
 }

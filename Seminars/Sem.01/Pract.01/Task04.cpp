ae91f04010409c7e32a10b2d91f0f356968f417c
Teodora Tsaneva <115184993+tedy11@users.noreply.github.com>
2023-03-01 20:36:09 +0200

    Task04

--- Seminars/Sem.01/Pract.01/Task04.cpp
+++ Seminars/Sem.01/Pract.01/Task04.cpp
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 ﻿#include <iostream>
 
 const short MAX_NAME_LEN = 30;
@@ -59,3 +60,76 @@ int main()
     //initStudent(refCSt, "PST New Name", 100);
 }
 
+=======
+#include <iostream>
+using std::cout;
+using std::cin;
+using std::endl;
+
+const short MAX_NEME_LEN = 30;
+
+struct Student
+{
+    char name[MAX_NEME_LEN + 1];
+    unsigned int fn;
+};
+
+void initStudent(Student& st, const char name[], unsigned int FN)
+{
+    cout << "Enter name: ";
+    cin.getline(st.name, 31);
+    cout << "Enter faculty number: ";
+    cin >> st.fn;
+    cin.ignore();
+}
+
+void printStudent(const Student& st)
+{
+    cout << "Name: " << st.name << endl;
+    cout << "faculty number: " << st.fn << endl;
+}
+
+int main()
+{
+    Student st = { "Gosho", 10 };
+    printStudent(st);
+    st = { "Ivan", 20 };
+    printStudent(st);
+    initStudent(st, "New Name", 82);
+    printStudent(st);
+    std::cout << endl;
+
+    Student* pSt = &st;
+    *pSt = { "Valeri", 30 };
+    printStudent(*pSt);
+    initStudent(*pSt, "PST New Name", 100);
+    printStudent(*pSt);
+    std::cout << endl;
+
+    const Student* pCSt = &st;
+    //*pCSt = { "Valeri", 30 };
+    //initStudent(*pCSt, "PST New Name", 100);
+
+    Student* const cPSt = &st;
+    *cPSt = { "Ivaylo", 1000 };
+    printStudent(*cPSt);
+    initStudent(*cPSt, "CPST New Name", 100);
+    printStudent(*cPSt);
+    std::cout << endl;
+
+    const Student* const cpCSt = &st;
+    //*cpCSt = { "Valeri", 30 };
+    //initStudent(*cpCSt, "PST New Name", 100);
+
+    Student& refSt = st;
+    refSt = { "Valentin", 82 };
+    printStudent(refSt);
+    initStudent(refSt, "REFST New Name", 100);
+    printStudent(refSt);
+
+    const Student& refCSt = st;
+    //refCSt = { "Valentin", 82 };
+    //initStudent(refCSt, "PST New Name", 100);
+}
+
+>>>>>>> Stashed changes

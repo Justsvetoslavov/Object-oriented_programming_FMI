fff6d0b2081c0a175fa08e828c11e77c18a78159
Teodora Tsaneva <115184993+tedy11@users.noreply.github.com>
2023-03-01 20:35:20 +0200

    Task02

--- Seminars/Sem.01/Pract.01/Task02.cpp
+++ Seminars/Sem.01/Pract.01/Task02.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 
+<<<<<<< Updated upstream
 using std::cin, std::cout, std::endl;
 
 void inputElements(int *arr, const int N) {
@@ -17,6 +18,33 @@ int countSymbol(const int *arr, const int N, const char symbol) {
         }
     }
     return count;
+=======
+int countSymbol(int* array, int size, char ch, int counter = 0)
+{
+	for (int i = 0; i < size; i++)
+	{
+		if (array[i] >= ch)
+			counter++;
+	}
+	return counter;
+}
+
+int main()
+{
+	int N;
+	cout << "Enter the number of elements in the array: ";
+	cin >> N;
+	int* array = new int[N];
+	for (int i = 0; i < N; i++)
+	{
+		cin >> array[i];
+	}
+	char symbol;
+	cout << "Enter the symbol: ";
+	cin >> symbol;
+	cout << countSymbol(array, N, symbol);
+	delete[] array;
+>>>>>>> Stashed changes
 }
 
 int main() {

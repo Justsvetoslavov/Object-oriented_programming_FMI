3344708c4f3c9d356646922f0502ef55d22c8585
gabrielavm <115610325+gabrielavm@users.noreply.github.com>
2023-03-02 00:22:18 +0200

    Update Task02.cpp

--- Seminars/Sem.01/Pract.01/Task02.cpp
+++ Seminars/Sem.01/Pract.01/Task02.cpp
@@ -1,38 +1,41 @@
+//Gabriela Mladenova
+//SI
+//Group 1
+//FN: 3MI0600225
+
 #include <iostream>
 
-using std::cin, std::cout, std::endl;
-
-void inputElements(int *arr, const int N) {
-    for (int i = 0; i < N; i++) {
-        cout << "Enter the " << i + 1 << " element: ";
-        cin >> arr[i];
-    }
-}
-
-int countSymbol(const int *arr, const int N, const char symbol) {
-    int count = 0;
-    for (int i = 0; i < N; i++) {
-        if (arr[i] > (int) symbol) {
-            count++;
+int countOfBiggerASCII(int* arr, int size, char symbol)
+{
+    int counter = 0;
+    for (int i = 0; i < size; ++i)
+    {
+        if (arr[i] > symbol)
+        {
+            ++counter;
         }
     }
-    return count;
+    return counter;
 }
-
-int main() {
+int main()
+{
     int N;
-    cout << "Enter the number of elements in the array: ";
-    cin >> N;
-
-    int *arr = new int[N];
-    inputElements(arr, N);
+    std::cout << "Enter number of codes: ";
+    std::cin >> N;
+    int* codes = new int[N];
+    for (int i = 0; i < N; ++i)
+    {
+        std::cin >> codes[i];
+    }
 
     char symbol;
-    cout << "Enter the symbol: ";
-    cin >> symbol;
+    std::cout << "Enter the symbol: ";
+    std::cin >> symbol;
 
-    cout << "The number of elements greater than the symbol: " << countSymbol(arr, N, symbol) << endl;
-    
-    delete[] arr;
+    std::cout << "The number of bigger codes than the code of the symbol is: ";
+    std::cout << countOfBiggerASCII(codes, N, symbol);
+
+    delete[] codes;
+
     return 0;
 }

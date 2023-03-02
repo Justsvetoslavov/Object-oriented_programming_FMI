0a99beb9bd84bd43dc03b6c35638b754efaacc67
Venelina Andreeva <venian2003@abv.bg>
2023-03-02 19:08:05 +0200

    Add Task04

--- Seminars/Sem.01/Pract.01/Task04.cpp
+++ Seminars/Sem.01/Pract.01/Task04.cpp
@@ -1,61 +1,28 @@
-﻿#include <iostream>
+#include <iostream>
 
-const short MAX_NAME_LEN = 30;
+const int NAME_SIZE = 30;
 
 struct Student {
-    char name[MAX_NAME_LEN + 1];
-    unsigned int FN;
+	char name[NAME_SIZE + 1];
+	unsigned int facultyNumber;
 };
 
-void initStudent(Student& student, const char name[], unsigned int FN) {
-    strcpy_s(student.name, MAX_NAME_LEN, name);
-    student.FN = FN;
+void InitStudent(Student& student) {
+	std::cout << "Enter student name: ";
+	std::cin.getline(student.name, 31);
+	std::cout << "Enter student faculty number: ";
+	std::cin >> student.facultyNumber;
 }
 
-void printStudent(const Student& student) {
-    std::cout << student.name << " " << student.FN << "\n";
+void PrintStudent(const Student& student) {
+	std::cout << "Name: " << student.name << std::endl;
+	std::cout << "Faculty number: " << student.facultyNumber << std::endl;
 }
 
 int main()
 {
-    Student st = { "Gosho", 10 };
-    printStudent(st);
-    st = {"Ivan", 20 };
-    printStudent(st);
-    initStudent(st, "New Name", 82);
-    printStudent(st);
-    std::cout << "\n\n";
+	Student myStudent;
 
-    Student* pSt = &st;
-    *pSt = { "Valeri", 30 };
-    printStudent(*pSt);
-    initStudent(*pSt, "PST New Name", 100);
-    printStudent(*pSt);
-    std::cout << "\n\n";
-
-    const Student* pCSt = &st;
-    //*pCSt = { "Valeri", 30 };
-    //initStudent(*pCSt, "PST New Name", 100);
-
-    Student* const cPSt = &st;
-    *cPSt = { "Ivaylo", 1000 };
-    printStudent(*cPSt);
-    initStudent(*cPSt, "CPST New Name", 100);
-    printStudent(*cPSt);
-    std::cout << "\n\n";
-
-    const Student* const cpCSt = &st;
-    //*cpCSt = { "Valeri", 30 };
-    //initStudent(*cpCSt, "PST New Name", 100);
-
-    Student& refSt = st;
-    refSt = {"Valentin", 82};
-    printStudent(refSt);
-    initStudent(refSt, "REFST New Name", 100);
-    printStudent(refSt);
-
-    const Student& refCSt = st;
-    //refCSt = { "Valentin", 82 };
-    //initStudent(refCSt, "PST New Name", 100);
+	InitStudent(myStudent);
+	PrintStudent(myStudent);
 }
-

33a8410b5cb5d309cc5c09c87db73172a9524979
Venelina Andreeva <venian2003@abv.bg>
2023-03-02 19:08:15 +0200

    Add Task05

--- Seminars/Sem.01/Pract.01/Task05.cpp
+++ Seminars/Sem.01/Pract.01/Task05.cpp
@@ -1,24 +1,41 @@
 #include <iostream>
-
-using std::cin, std::cout, std::endl;
+using std::cin;
+using std::cout;
+using std::endl;
 
 struct ComplexNumber {
-    double real;
-    double imaginary;
+    int realPart;
+    int imaginaryPart;
 };
 
-ComplexNumber additionOfComplexNumbers(const ComplexNumber &lhs, const ComplexNumber &rhs) {
-    return {lhs.real + rhs.real, lhs.imaginary + rhs.imaginary};
+ComplexNumber SumTwoComplexNumbers(const ComplexNumber& num1, const ComplexNumber& num2) {
+    ComplexNumber product;
+
+    product.realPart = num1.realPart + num2.realPart;
+    product.imaginaryPart = num1.imaginaryPart + num2.imaginaryPart;
+
+    return product;
 }
 
-void printComplexNumber(const ComplexNumber &number) {
-    cout << number.real << " + " << number.imaginary << "i" << endl;
+void ReadComplexNumber(ComplexNumber& num) {
+    std::cout << "Enter real part: ";
+    std::cin >> num.realPart;
+    std::cout << "Enter imaginary part: ";
+    std::cin >> num.imaginaryPart;
 }
 
-int main() {
-    ComplexNumber a = {1.0, 2.0};
-    ComplexNumber b = {3.0, 4.0};
-    ComplexNumber c = additionOfComplexNumbers(a, b);
-    printComplexNumber(c);
-    return 0;
+void PrintComplexNumber(const ComplexNumber& num) {
+    std::cout << "Complex number is: " << num.realPart
+        << "+" << num.imaginaryPart << "i" << std::endl;
+}
+
+int main()
+{
+    ComplexNumber num1, num2;
+    ReadComplexNumber(num1);
+    ReadComplexNumber(num2);
+
+    ComplexNumber product = SumTwoComplexNumbers(num1, num2);
+    PrintComplexNumber(product);
+
 }

e207ba6bb51c9ccd093cde6c75f4b5599bffef71
ivan9643 <88584871+ivan9643@users.noreply.github.com>
2023-03-02 19:57:41 +0200

    Create Task02.cpp

---
+++ Seminars/Sem.02/Pract.02/Task02.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <fstream>
+
+using std::cout;
+using std::endl;
+using std::ifstream;
+
+int CharArrayLength(char*);
+int LengthOfFile(const char*);
+
+int main() {
+	
+}
+
+int CharArrayLength(char* arr) {
+	int length = 0;
+	while (*arr++ != '\0') length++;
+	return length;
+}
+
+int LengthOfFile(const char* fileName) {
+	ifstream fileToRead(fileName);
+	if (!fileToRead.is_open())
+	{
+		cout << "Couldn't open " << fileName << endl;
+		return 0;
+	}
+	int result = 0;
+	while (!fileToRead.eof())
+	{
+		char buffer[1024];
+		fileToRead.getline(buffer, 1024);
+		result += CharArrayLength(buffer);
+	}
+	return result;
+}

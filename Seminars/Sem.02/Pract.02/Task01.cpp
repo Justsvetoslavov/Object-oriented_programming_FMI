69520a4a6461277ad7d8eedb6fcf88d2b0d359d3
ivan9643 <88584871+ivan9643@users.noreply.github.com>
2023-03-02 19:57:34 +0200

    Create Task01.cpp

---
+++ Seminars/Sem.02/Pract.02/Task01.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <fstream>
+
+using std::cin; 
+using std::cout;
+using std::endl;
+using std::ofstream;
+using std::ifstream;
+
+void PrintFileContent(const char*);
+
+int main() {
+	int a, b, c;
+	cin >> a >> b >> c;
+	ofstream fileToWrite("result.txt");
+	int product = a * b * c;
+	int sum = a + b + c;
+	fileToWrite << "Product: " << product << endl;
+	fileToWrite << "Sum: " << sum << endl;
+	const char* filename = "result.txt";
+	PrintFileContent(filename);
+}
+
+void PrintFileContent(const char* fileName) {
+	ifstream fileToRead(fileName);
+	if (!fileToRead.is_open())
+	{
+		cout << "Couldn't open " << fileName << endl;
+		return;
+	}
+	while (!fileToRead.eof())
+	{
+		char buffer[128];
+		fileToRead.getline(buffer, 128);
+		cout << buffer << endl;
+	}
+}

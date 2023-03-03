193571e5ddc904b96cf853c502f00afae1c58876
gabrielavm <115610325+gabrielavm@users.noreply.github.com>
2023-03-04 01:29:12 +0200

    Update Task07.cpp

--- Seminars/Sem.01/Pract.01/Task07.cpp
+++ Seminars/Sem.01/Pract.01/Task07.cpp
@@ -2,16 +2,18 @@
 //SI
 //Group 1
 //FN: 3MI0600225
+
 #include <iostream>
 
 const int SIZEOFARRAYS = 30;
-enum MovieGenre
+enum class MovieGenre
 {
     Comedy,
     Action,
     Horror,
     Romantic
 };
+
 struct Movie
 {
     char movieName[SIZEOFARRAYS + 1];
@@ -19,6 +21,7 @@ struct Movie
     MovieGenre movieGenre;
     double rating;
 };
+
 void printMovies(const Movie movies[], const int SIZE)
 {
     for (int i = 0; i < SIZE; ++i)
@@ -27,35 +30,35 @@ void printMovies(const Movie movies[], const int SIZE)
         std::cout << "Duration of the movie: " << movies[i].duration << std::endl;
         std::cout << "Movie genre: ";
 
-        if (MovieGenre::Comedy)
+        switch(movies[i].movieGenre)
         {
+        case MovieGenre::Comedy:
             std::cout << "Comedy" << std::endl;
-        }
-        else if (MovieGenre::Action)
-        {
+            break;
+
+        case MovieGenre::Action:
             std::cout << "Action" << std::endl;
-        }
-        else if (MovieGenre::Horror)
-        {
+            break;
+
+        case MovieGenre::Horror:
             std::cout << "Horror" << std::endl;
-        }
-        else if (MovieGenre::Romantic)
-        {
+            break;
+
+        case MovieGenre::Romantic:
             std::cout << "Romantic" << std::endl;
-        }
-        else
-        {
-            std::cout << "No genre" << std::endl;
+            break;
         }
 
         std::cout << "Movie rating: " << movies[i].rating << std::endl;
         std::cout << std::endl;
     }
 }
+
 void PrintTopMovie(const Movie movies[], const int SIZE)
 {
     int topMovieIndex = 0;
-    int topRating = 0;
+    double topRating = 0;
+
     for (int i = 0; i < SIZE; ++i)
     {
         if (movies[i].rating > topRating)
@@ -64,40 +67,43 @@ void PrintTopMovie(const Movie movies[], const int SIZE)
             topMovieIndex = i;
         }
     }
-    for (int j = 0; j < SIZE; ++j)
-    {
-        if (j == topMovieIndex)
-        {
-            std::cout << movies[j].movieName << " " << movies[j].duration
-                << " " << movies[j].movieGenre << " " << movies[j].rating;
-        }
-    }
+    std::cout << movies[topMovieIndex].movieName << " " << movies[topMovieIndex].duration;
 }
+
 void swapMovies(Movie movies[], const int firstIndex, const int secondIndex)
 {
     Movie temp = movies[firstIndex];
     movies[firstIndex] = movies[secondIndex];
     movies[secondIndex] = temp;
 }
+
 void sortMoviesByRating(Movie movies[], const int SIZE)
 {
-    for (int i = 0; i < SIZE - 1; ++i)
+    int i, j, minIndex;
+    for (i = 0; i < SIZE - 1; ++i)
     {
-        for (int j = 0; j < SIZE - i - 1; ++j)
+        minIndex = i;
+
+        for (j = i + 1; j < SIZE; ++j)
         {
-            if (movies[j].rating < movies[j + 1].rating)
+            if (movies[j].rating > movies[minIndex].rating)
             {
-                swapMovies(movies, j, j + 1);
+                minIndex = j;
             }
+        }
+        if (minIndex != i)
+        {
+            swapMovies(movies, minIndex, i);
         }
     }
 }
+
 int main()
 {
     const int SIZE = 3;
-    Movie movies[SIZE] = { "movieName1", 150, Comedy, 5,
-    "movieName2", 100, Action, 20,
-    "movieName3", 120, Horror, 9};
+    Movie movies[SIZE] = { "movieName1", 150, MovieGenre::Comedy, 5,
+    "movieName2", 100, MovieGenre::Action, 20,
+    "movieName3", 120, MovieGenre::Horror, 9};
     std::cout << "The top movie is: ";
     PrintTopMovie(movies, SIZE);
     std::cout << std::endl;

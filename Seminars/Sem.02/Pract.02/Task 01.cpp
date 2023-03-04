d6446ee02db85e12178dd5ee8380772801a4a9f5
Natalia <115610226+NataliaN24@users.noreply.github.com>
2023-03-04 23:58:13 +0200

    Create Task 01.cpp
    Natalia Nakollofski  SI 1  1MI8000034

---
+++ Seminars/Sem.02/Pract.02/Task 01.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+#include<fstream>
+using std::cin;
+using std::cout;
+using std::endl;
+
+
+void saveToFile() {
+	double a, b, c;
+	cin >> a >> b >> c;
+	double sum = a + b + c;
+
+	double product = a * b * c;
+	std::ofstream file("result.txt");
+	if (!file.is_open()) {
+		cout << "Error!";
+		return ;
+	}
+	file << sum << ' ' << product;
+	file.close();
+
+}
+void Difference() {
+
+	std::ifstream file("result.txt");
+	double sum, product;
+	if (!file.is_open()) {
+		cout << "Error!";
+		return;
+	}
+	file >> sum >> product;
+	double difference = product-sum;
+
+	cout << "The difference is:" << difference;
+	file.close();
+
+}
+
+
+int main() {
+	saveToFile();
+	Difference();
+}

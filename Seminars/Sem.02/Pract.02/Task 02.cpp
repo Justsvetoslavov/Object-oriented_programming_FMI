2e3d53e85eefc5c804a2deb607ebc9839e9ae272
Natalia <115610226+NataliaN24@users.noreply.github.com>
2023-03-05 00:00:01 +0200

    Create Task 02.cpp

---
+++ Seminars/Sem.02/Pract.02/Task 02.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include<fstream>
+using std::cin;
+using std::cout;
+using std::endl;
+
+
+size_t lengthOfFile(const char *name) {
+
+	std::ifstream file(name);
+	if (!file.is_open()) {
+		cout << "Error!";
+		return -1;
+	}
+	file.seekg(0, std::ios::end);
+	size_t Position = file.tellg();
+
+	file.close();
+	return Position;
+
+}
+
+
+int main() {
+	cout << lengthOfFile("result.txt");
+}

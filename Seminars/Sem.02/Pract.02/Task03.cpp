d302d81fb78dde367a46bb59fd6eaf1053be7dee
Vasil Paligorov <36786687+VasilPaligorov@users.noreply.github.com>
2023-03-04 17:31:59 +0200

    Update Task03.cpp

--- Seminars/Sem.02/Pract.02/Task03.cpp
+++ Seminars/Sem.02/Pract.02/Task03.cpp
@@ -3,23 +3,23 @@
 
 using std::cin, std::cout, std::endl, std::ofstream, std::ifstream;
 
-const char* valuesFile = "values.txt";
-const char* placeholderFile = "placeholder.txt";
-const char* resultsFile = "results.txt";
+const char* VALUES_FILE = "../values.txt";
+const char* PLACEHOLDER_FILE = "../placeholder.txt";
+const char* RESULTS_FILE = "../results.txt";
 
-void getValuesFromFile(ifstream &file, char values[][1024]) {
+void getValuesFromFile(ifstream &valuesFile, char values[][1024]) {
     char type[1024];
-    while (!file.eof()) {
-        file >> type;
-        file.ignore(1, ' ');
+    while (!valuesFile.eof()) {
+        valuesFile >> type;
+        valuesFile.ignore(1, ' ');
         if (strcmp(type, "title") == 0) {
-            file.getline(values[0], 1024);
+            valuesFile.getline(values[0], 1024);
         } else if (strcmp(type, "recipient_name") == 0) {
-            file.getline(values[1], 1024);
+            valuesFile.getline(values[1], 1024);
         } else if (strcmp(type, "message") == 0) {
-            file.getline(values[2], 1024);
+            valuesFile.getline(values[2], 1024);
         } else if (strcmp(type, "sender_name") == 0) {
-            file.getline(values[3], 1024);
+            valuesFile.getline(values[3], 1024);
         } else {
             cout << "Unknown type: " << type << endl;
             cout << "Please check the file" << endl;
@@ -51,43 +51,44 @@ void replaceWordInString(char *line, const char *word, const char *value) {
     }
 }
 
-void writeToFile(ifstream &placeholder, ofstream &result, const char values[][1024]) {
+void writeToFile(ifstream &placeholderFile, ofstream &resultsFile, const char values[][1024]) {
     char line[1024];
-    while (!placeholder.eof()) {
-        placeholder.getline(line, 1024);
+    while (!placeholderFile.eof()) {
+        placeholderFile.getline(line, 1024);
         replaceWordInString(line, "{title}", values[0]);
         replaceWordInString(line, "{recipient_name}", values[1]);
         replaceWordInString(line, "{message}", values[2]);
         replaceWordInString(line, "{sender_name}", values[3]);
-        result << line << endl;
+        resultsFile << line << endl;
     }
 }
 
 int main() {
     char values[4][1024];
-    ifstream file(valuesFile);
-    if (!file.is_open()) {
+    ifstream valuesFile(VALUES_FILE);
+    if (!valuesFile.is_open()) {
         cout << "Values is not open" << endl;
         return -1;
     }
-    getValuesFromFile(file, values);
-    file.close();
+    getValuesFromFile(valuesFile, values);
+    valuesFile.close();
 
-    ofstream result(resultsFile);
-    if (!result.is_open()) {
+    ofstream resultsFile(RESULTS_FILE);
+    if (!resultsFile.is_open()) {
         cout << "Results is not open" << endl;
         return -1;
     }
-    
-    ifstream placeholder(placeholderFile);
-    if (!placeholder.is_open()) {
+
+    ifstream placeholderFile(PLACEHOLDER_FILE);
+    if (!placeholderFile.is_open()) {
         cout << "Placeholder is not open" << endl;
         return -1;
     }
-    
-    writeToFile(placeholder, result, values);
-    placeholder.close();
-    result.close();
-    
+
+    writeToFile(placeholderFile, resultsFile, values);
+
+    placeholderFile.close();
+    resultsFile.close();
+
     return 0;
 }

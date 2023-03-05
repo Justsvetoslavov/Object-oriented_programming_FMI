fecbf2e29abf6ead535bfb15e4f5a326348236ba
raiagrigorova <raiagrigorova@gmail.com>
2023-03-06 00:30:58 +0200

    Create Task02.cpp

---
+++ Seminars/Sem.02/Pract.02/Task02.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <fstream>
+
+const char EXAMPLE_FILE_NAME[] = "example.txt";
+
+void initExampleFile() {
+    std::ofstream myFile(EXAMPLE_FILE_NAME);
+
+    if (!myFile.is_open()) {
+        return;
+    }
+
+    myFile << "dhjdicnjduwjhujhujhjsuhufjhdhjhuejhdeu27727277772727272232dfsdsfssfddsfs"; //72
+
+    myFile.close();
+}
+
+int lengthOfFile(const char fileName[]) {
+    std::ifstream myFile(fileName);
+
+    if (!myFile.is_open()) {
+        return -1;
+    }
+
+    myFile.seekg(0, myFile.end);
+    return myFile.tellg();
+}
+
+int main()
+{
+    initExampleFile();
+    std::cout << "Length of file: " << lengthOfFile(EXAMPLE_FILE_NAME);
+}

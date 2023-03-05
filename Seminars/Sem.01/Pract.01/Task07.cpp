b9f85966f3d07b57bba2bb07c27395f2a8f4bde7
Dimityr Nikolov <dimitrnikolov710@gmail.com>
2023-03-05 14:37:57 +0200

    Update Task07.cpp

--- Seminars/Sem.01/Pract.01/Task07.cpp
+++ Seminars/Sem.01/Pract.01/Task07.cpp
@@ -1,117 +1,89 @@
+// ConsoleApplication2.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
 #include <iostream>
-
-const int MAX_NAME_SIZE = 30;
-
-enum class Genre
-{
-	Comedy,
-	Action,
-	Horror,
-	Romantic
+using std::cout;
+using std::cin;
+using std::endl;
+struct Movies {
+	char name[40];
+	double time,rating;
+	short Genre;
+	enum genre { Comedy, Action, Horror, Romantic };
 };
-
-struct Movie
-{
-	double rating;
-	Genre genre;
-	int duration;
-	char name[MAX_NAME_SIZE + 1];
-};
-
-void Swap(Movie& lhs, Movie& rhs)
-{
-	Movie temp = lhs;
-	lhs = rhs;
-	rhs = temp;
-}
-
-void StrCopy(char* dest, const char* source)
-{
-	size_t index = 0;
-	while (source[index] != '\0') {
-		dest[index] = source[index];
-		index++;
-	}
-  
-	arr1[index] = '\0';
-}
-
-void InitMovie(Movie& movie, const char* name, const double rating, const int duration, const Genre genre)
-{
-	StrCopy(movie.name, name);
-	movie.rating = rating;
-	movie.duration = duration;
-	movie.genre = genre;
-}
-
-void PrintMovies(const Movie* moviesArr, const int count)
-{
-	for (size_t i = 0; i < count; i++) {
-		std::cout << "Movie name: " << moviesArr[i].name << std::endl;
-		std::cout << "Duration: " << moviesArr[i].duration << std::endl;
-		std::cout << "Rating: " << moviesArr[i].rating << std::endl;
-		std::cout << "Genre: ";
-		switch (moviesArr[i].genre) {
-			case Genre::Comedy: std::cout << "Comedy"; break;
-			case Genre::Action: std::cout << "Action"; break;
-			case Genre::Horror: std::cout << "Horror"; break;
-			case Genre::Romantic: std::cout << "Romantic"; break;
+void createMovies(Movies*& movies, int numberOfMovies) {
+	for (size_t i = 3; i < numberOfMovies+3; i++) {
+		cout << "Name: ";
+		cin.getline(movies[i].name,20);
+		cin.ignore(1, '\n');
+		cout << "How long is the movie: ";
+		cin >> movies[i].time;
+		cout << "Rating of the movie: ";
+		cin >> movies[i].rating;
+		cout << "Based on the genres enter a number: 0-Comedy , 1-Action , 2-Horror , 3-Romantic";
+		cin >> movies[i].Genre;
+	 
+		while (movies[i].Genre < 0 || movies[i].Genre > 3) {
+				cin >> movies[i].Genre;			
 		}
-		std::cout << std::endl;
 	}
 }
-
-void SortMoviesByRating(Movie* movies, const int count)
-{
-	for (size_t i = 0; i < count - 1; ++i) {
-		size_t indexOfMaxElement = i;
-		for (size_t j = i + 1; j < count; ++j) {
-			if (movies[indexOfMaxElement].rating < movies[j].rating) {
-				indexOfMaxElement = j;
+void sortMovies(Movies*& movies, int numberOfMovies) {
+	for (int i = 0; i < numberOfMovies; i++) {
+		for (int j = i+1; j < numberOfMovies; j++) {
+			if (movies[i].rating > movies[j].rating) {
+				Movies temp = movies[i];
+				movies[i] = movies[j];
+				movies[j] = temp;
+				
 			}
-		}
-		
-		if(indexOfMaxElement != i) {
-			Swap(movies[i], movies[indexOfMaxElement]);
+			
 		}
 	}
-  
 }
-
-void PrintNameOfBestRatedMovie(const Movie* movies, const int count)
-{
-	int indexOfBest = 0;
-
-	for (size_t i = 1; i < count; i++) {
-		if (movies[indexOfBest].rating < movies[i].rating) {
-			indexOfBest = i;
+void printMovies(Movies* movies, int numberOfMovies) {
+	for (int i = 0; i < numberOfMovies+3; i++) {
+		cout << "Name:" << movies[i].name<<endl;
+		cout << "Runtime:" << movies[i].time << " minutes"<<endl;
+        cout << "Rating:" << movies[i].rating<<endl;	
+		cout << "Genre:";
+		if (movies[i].Genre == 0) {
+			cout << "Comedy";
 		}
+		if (movies[i].Genre == 1) {
+			cout << "Action";
+		}
+		if (movies[i].Genre == 2) {
+			cout << "Horror";
+		}
+		if (movies[i].Genre == 3) {
+			cout << "Romantic";
+		}
+		cout << endl;
 	}
-
-	std::cout << movies[indexOfBest].name << std::endl;
 }
-
 int main()
 {
 	int numberOfMovies;
-	std::cin >> numberOfMovies;
+	cout << "Enter the number of movies you want to review: ";
+	cin >> numberOfMovies;
+	Movies* Movie = new Movies[numberOfMovies + 3];
+	Movie[0] = { "The Batman",176,8.7,1 };
+	Movie[1] = { "Twilight",122,5.3,3 };
+	Movie[2] = { "The thing",109,8.2,2 };
+	createMovies(Movie, numberOfMovies);
+	sortMovies(Movie, numberOfMovies);
+	printMovies(Movie, numberOfMovies);
 
-	Movie* movies = new Movie[numberOfMovies];
-
-	for (size_t i = 0; i < numberOfMovies; i++) {
-		char name[MAX_NAME_SIZE + 1];
-		std::cin >> name;
-		std::cin.ignore();
-		double rating;
-		int duration, type;
-		std::cin >> rating >> duration >> type;
-		Genre genre = (Genre)type; // TODO: validation / switch case
-		InitMovie(movies[i], name, rating, duration, genre);
-	}
-
-	PrintNameOfBestRatedMovie(movies, numberOfMovies);
-	SortMoviesByRating(movies, numberOfMovies);
-	PrintMovies(movies, numberOfMovies);
-  
-  	delete[] movies;
 }
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file

cbdd098d812105be6106db6b5f76f10030c032e2
raiagrigorova <raiagrigorova@gmail.com>
2023-03-06 00:48:35 +0200

    Create Task06.cpp

---
+++ Seminars/Sem.02/Pract.02/Task06.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <fstream>
+
+const int BUFF_SIZE = 1024;
+
+void printFile(const char fileName[]) {
+    std::ifstream file(fileName);
+
+    if (!file.is_open()) {
+        std::cout << "Error\n";
+        return;
+    }
+
+    while (!file.eof()) {
+        char buff[BUFF_SIZE];
+        file.getline(buff, 1024);
+        std::cout << buff << "\n";
+    }
+
+    file.close();
+}
+
+int main()
+{
+    printFile("Task06.cpp");
+}
+

c067cd1259be10be1919a87424f5538769995a73
Dimityr Nikolov <dimitrnikolov710@gmail.com>
2023-03-05 14:34:20 +0200

    Update Task06.cpp

--- Seminars/Sem.01/Pract.01/Task06.cpp
+++ Seminars/Sem.01/Pract.01/Task06.cpp
@@ -1,77 +1,79 @@
+// ConsoleApplication33.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
 #include <iostream>
-#include <cmath>
-
-const double EPSILON = 0.0001;
-
-struct Point
-{
-    double x = 0;
-    double y = 0;
+using namespace std;
+struct Point {
+	int x;
+	int y;
 };
-
-Point InitPoint(Point& point)
+double Lenght(const Point& p1, const Point& p2) {
+	return sqrt((p1.x - p2.x) * (p1.x - p2.y) + (p1.y - p2.y) * (p1.y - p2.y));
+}
+double lenghtFromCenter(const Point& p1) {
+	return sqrt((p1.x) * (p1.x) + (p1.y) * (p1.y));
+}
+void quadrantPlacement(const Point& p) {
+	if (p.x == 0 && p.y == 0) {
+		cout << "The Point is in the center."<<endl;
+	}
+	if (p.x < 0 && p.y < 0) {
+		cout << "The Third Point is in the third quadrant" << endl;
+	}
+	if (p.x > 0 && p.y > 0) {
+		cout << "The Third Point is in the first quadrant" << endl;
+	}
+	if (p.x > 0 && p.y < 0) {
+		cout << "The Third Point is in the fouth quadrant" << endl;
+	}
+	if (p.x < 0 && p.y > 0) {
+		cout << "The Third Point is in the second quadrant" << endl;
+	}
+}
+void radius(const Point& k,const int radius) {
+	if (k.x > radius || k.y > radius) {
+		cout << "Point K doesn't belong to the area.";
+	} 
+	else {
+		cout << "Point K belongs to the area.";
+	}
+}
+int main()
 {
-    std::cin >> point.x >> point.y;
+	Point p1, p2, p3,K;
+	int r;
+	cout << "Enter the cordinates for the first point : x = ";
+	cin >> p1.x;
+	cout << "y = ";
+	cin >> p1.y;
+	cout << "Enter the cordinates for the first point : x = ";
+	cin >> p2.x;
+	cout << "y = ";
+	cin >> p2.y;
+	cout << "Enter the coordinates for a third point: x = ";
+	cin >> p3.x;
+	cout << "y = ";
+	cin >> p3.y;
+	cout << "Enter the coordinates for point K: x = ";
+	cin >> K.x;
+	cout << "y = ";
+	cin >> K.y;
+	cout << "Enter a radius for point K : r = ";
+	cin >> r;
+	cout << "The lenght between the two points is " << Lenght(p1, p2) << " cms" << endl;
+	cout << "The lenght between the center and the first point is " << lenghtFromCenter(p1) << " cms" << endl;
+	cout << "The lenght between the center and the second point is " << lenghtFromCenter(p2) << " cms" << endl;
+	quadrantPlacement(p3);
+	radius(K, r);
 }
 
-void PrintPoint(const Point& point)
-{
-    std::cout << "(" << point.x << ", " << point.y << ")" << std::endl;
-}
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
 
-double GetDistFromCenter(const Point& point)
-{
-    return sqrt(point.x * point.x + point.y * point.y);
-}
-
-double GetDistBetweenPoints(const Point &f, const Point &s)
-{
-    //   ______________________________________________
-    // \/ (x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1)
-    // https://en.wikipedia.org/wiki/Euclidean_distance
-    double dx = f.x - s.x;
-    double dy = f.y - s.y;
-    return sqrt(dx * dx + dy * dy);
-}
-
-unsigned short GetQuadrant(const Point& point)
-{
-    if (point.x > 0 && point.y > 0) {
-        return 1;
-    } else if (point.x < 0 && point.y > 0) {
-        return 2;
-    } else if (point.x < 0 && point.y < 0) {
-        return 3;
-    } else if (point.x > 0 && point.y < 0) {
-        return 4;
-    }
-    
-    return 0;
-}
-
-bool IsPointInCircle(const double radius, const Point& point)
-{
-    bool pointIsInCircle = false;
-    double distFromCenter = getDistFromCenter(point);
-
-    if (abs(distFromCenter - radius) < EPSILON) {
-        std::cout << "Point lies on the circle";
-    } else {
-        if (distFromCenter > radius) {
-            std::cout << "Point lies outside the circle\n";
-            pointIsInCircle = true;
-        } else {
-            std::cout << "Point lies inside the circle\n";
-        }
-    }
-
-    return pointIsInCircle;
-}
-
-int main() {
-    Point point;
-    InitPoint(point);
-    PrintPoint(point);
-
-    IsPointInCircle(5, point);
-}
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file

722ab0609a1386e8698abab86231e966a35d0eea
Teodora Tsaneva <115184993+tedy11@users.noreply.github.com>
2023-03-06 21:57:14 +0200

    Task01

---
+++ Task01.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <fstream>
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::ifstream;
+using std::ofstream;
+
+void print(ifstream& readFile)
+{
+    int sum, product;
+    readFile >> sum >> product;
+    cout << abs(sum - product);
+}
+
+int main()
+{
+    int a, b, c;
+    cin >> a >> b >> c;
+    ofstream writeFile("result.txt");
+    if (!writeFile.is_open())
+    {
+        cout << "Error";
+        return -1;
+    }
+    int sum = a + b + c;
+    int product = a * b * c;
+    writeFile << sum << endl << product;
+    writeFile.close();
+    ifstream readFile("result.txt");
+    print(readFile);
+    readFile.close();
+}
+

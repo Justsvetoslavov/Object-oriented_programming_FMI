f9708eb2db4b34ee8165c42655007628c14862c7
IHaveNoIdeaWhatImDo-ing <84041839+IHaveNoIdeaWhatImDo-ing@users.noreply.github.com>
2023-03-08 11:14:56 -0800

    Update Task03.cpp

--- Seminars/Sem.02/Pract.02/Task03.cpp
+++ Seminars/Sem.02/Pract.02/Task03.cpp
@@ -1,23 +1,114 @@
 #include <iostream>
 #include <fstream>
+#include <sstream>
 
 using namespace std;
 
-const int MAX_SIZE = 256;
-char ifpath[MAX_SIZE] = "C:\\Users\\boris\\OneDrive\\Desktop\\inFile.txt";
-char ofpath[MAX_SIZE] = "C:\\Users\\boris\\OneDrive\\Desktop\\outFile.txt";
+const int MAX_SIZE = 128;
+
+// The path to the file with the values to replace
+char ifpath[MAX_SIZE]      = "C:\\Users\\boris\\OneDrive\\Desktop\\data.txt";
+// The path to the blank of the report
+char ofpath[MAX_SIZE]      = "C:\\Users\\boris\\OneDrive\\Desktop\\report.txt";
+// The path to the report
+char patternpath[MAX_SIZE] = "C:\\Users\\boris\\OneDrive\\Desktop\\pattern.txt";
+
+char patterns[MAX_SIZE][MAX_SIZE];
+char elements[MAX_SIZE][MAX_SIZE];
+
+int length(string str)
+{
+	int i = 0;
+	while (str[i++] != '\0');
+	return i - 1;
+}
+
+void strCopy(string& str, char chr[MAX_SIZE], int len)
+{
+	for (int i = 0; i < len; ++i)
+	{
+		chr[i] = str[i];
+	}
+}
+
+bool strComp(string line, char pattern[MAX_SIZE])
+{
+	int i = 0;
+	while (pattern[i] != '\0')
+		if (line[i] != pattern[i++])
+			return false;
+
+	return true;
+}
+
+void readData()
+{
+	ifstream streamReader(ifpath);
+	int i = 0;
+
+	if (streamReader.is_open())
+	{
+		while (streamReader.good() && !streamReader.eof())
+		{
+			char str[MAX_SIZE];
+			streamReader.getline(str, MAX_SIZE);
+			string temp, concat;
+			stringstream strStream(str);
+
+			getline(strStream, temp, ' ');
+			strCopy(temp, patterns[i], length(temp));
+
+			while (getline(strStream, temp, ' '))
+			{
+				concat += temp + ' ';
+			}
+			strCopy(concat, elements[i++], length(concat));
+			elements[i - 1][length(elements[i - 1]) - 1] = '\0';
+		}
+	}
+
+	streamReader.close();
+}
 
 void createReport(void)
 {
-	ifstream streamReader(ifpath);
-	ofstream streamWriter(ofpath);
+	ifstream streamReader(patternpath);
+	ofstream streamWriter(ofpath, ofstream::app);
 
-	char temp, elements[4][MAX_SIZE];
+	if (streamReader.is_open())
+	{
+		while (streamReader.good() && !streamReader.eof())
+		{
+			char str[MAX_SIZE];
+			streamReader.getline(str, MAX_SIZE);
+			string temp;
+			stringstream strStream(str);
 
-	for (short i = 0, index = 0; i < 4; ++i)
-		streamReader.getline(elements[i], 256);
+			while (getline(strStream, temp, '{'))
+			{
+				string temp2;
+				stringstream strStream2(temp);
 
-	streamWriter << "Dear, " << elements[0] << " " << elements[1] << ".\n" << '\n' << elements[2] << '\n' << '\n' << "Sincirely,\n" << elements[3];
+				while (getline(strStream2, temp2, '}'))
+				{
+					bool isPattern = false;
+					for (int i = 0; i < 4; ++i)
+					{
+						if (strComp(temp2, patterns[i]))
+						{
+							streamWriter << elements[i];
+							isPattern = true;
+							break;
+						}
+					}
+
+					if (!isPattern) streamWriter << temp2;
+				}
+			}
+
+			streamWriter << '\n';
+		}
+	}
 
 	streamReader.close();
 	streamWriter.close();
@@ -25,6 +116,7 @@ void createReport(void)
 
 int main()
 {
+	readData();
 	createReport();
 
 	return 0;

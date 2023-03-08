7b34393408022971c56172f79b7c39299206c246
NayleG <115502650+NayleG@users.noreply.github.com>
2023-03-08 15:04:08 +0200

    Task01

--- Seminars/Sem.01/Pract.01/Task01.cpp
+++ Seminars/Sem.01/Pract.01/Task01.cpp
@@ -1,26 +1,68 @@
 #include <iostream>
+using std:: cin;
+using std:: cout;
 
-int LowerNumberByCopy(const int num1, const int num2)
+int smallerValue(int a, int b)
 {
-	return num1 < num2 ? num1 : num2;
+    if(a<b)
+    {
+        return a;
+    }
+    return b;
+}
+int smallerValueConst(const int a, const int b)
+{
+    if(a<b)
+    {
+        return a;
+    }
+
+    return b;
 }
 
-int LowerNumberByReference(const int& num1, const int& num2) 
+int smallerPionter(const int* FN, const int* SN)
 {
-	return num1 < num2 ? num1 : num2;
+    if((*FN)<(*SN))
+    {
+        return (*FN);
+    }
+
+    return (*SN);
 }
 
-int LowerNumberByPointer(const int* num1, const int* num2) 
+int smallerReference(const int& FIRSTNUM, const int& SECONDNUM)
 {
-	return *num1 < *num2 ? *num1 : *num2;
+    if(FIRSTNUM<SECONDNUM)
+    {
+        return FIRSTNUM;
+    }
+
+    return SECONDNUM;
 }
 
-int main() 
+int main()
 {
-	int num1, num2;
-	std::cin >> num1 >> num2;
+    int a,b;
+    cout << "a = ";
+    cin >> a;
+    cout << "b = ";
+    cin >> b;
+    const int FIRSTNUM = 37, SECONDNUM = 96;
+    const int* FN = &FIRSTNUM;
+    const int* SN = &SECONDNUM;
+    int* pa = &a;
+    int* pb = &b;
+    cout << "Min number by Value: " << smallerValue(FIRSTNUM, SECONDNUM) << '\n';
+    cout << "Min number by Value: " << smallerValue(a, b) << '\n';
+    cout << "Min number by Value: " << smallerValueConst(FIRSTNUM, SECONDNUM) << '\n';
+    cout << "Min number by Value: " << smallerValueConst(a,b) << '\n';
 
-	std::cout << LowerNumberByCopy(num1, num2) << std::endl;
-	std::cout << LowerNumberByReference(num1, num2) << std::endl;
-	std::cout << LowerNumberByPointer(&num1, &num2) << std::endl;
+    //cout << "Min number by Pointer: " << smallerPionter(FIRSTNUM, SECONDNUM) << '\n'; -----> won't work
+    cout << "Min number by Pointer: " << smallerPionter(FN, SN) << '\n';
+    cout << "Min number by Pointer: " << smallerPionter(pa, pb) << '\n';
+    //cout << "Min number by Reference: " << smallerPionter(a, b) << '\n';  ------> won't work
+
+    //cout << "Min number by Reference: " << smallerReference(FN, SN) << '\n'; -----> wont work
+    cout << "Min number by Reference: " << smallerReference(a, b) << '\n';
+    cout << "Min number by Reference: " << smallerReference(FIRSTNUM, SECONDNUM) << '\n';
 }

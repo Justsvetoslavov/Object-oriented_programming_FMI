cad9a580f2ef041f209b63fb684a1c73757b1f03
NayleG <115502650+NayleG@users.noreply.github.com>
2023-03-11 14:40:08 +0200

    Task.07

--- Seminars/Sem.01/Pract.01/Task07.cpp
+++ Seminars/Sem.01/Pract.01/Task07.cpp
@@ -1,117 +1,151 @@
 #include <iostream>
+using std:: cout;
+using std:: cin;
 
-const int MAX_NAME_SIZE = 30;
+const int MAX_LENGHT = 30;
 
 enum class Genre
 {
-	Comedy,
-	Action,
-	Horror,
-	Romantic
+    Comedy,
+    Thriller,
+    Drama, 
+    Horror
 };
 
 struct Movie
 {
-	double rating;
-	Genre genre;
-	int duration;
-	char name[MAX_NAME_SIZE + 1];
+    char movieName[MAX_LENGHT];
+    int time;
+    Genre genre;
+    double rating;
 };
 
-void Swap(Movie& lhs, Movie& rhs)
+void saveName(const char* name, char* movieName)
 {
-	Movie temp = lhs;
-	lhs = rhs;
-	rhs = temp;
+    int br = 0;
+    while(name[br] != '\0')
+    {
+        movieName[br] = name[br];
+        br++;
+    }
+
+    movieName[br] = '\0';
+
 }
 
-void StrCopy(char* dest, const char* source)
+void createMovie(Movie& movie, const char* name, const int& time, const Genre& genre, const double& rating)
 {
-	size_t index = 0;
-	while (source[index] != '\0') {
-		dest[index] = source[index];
-		index++;
-	}
-  
-	arr1[index] = '\0';
+    saveName(name, movie.movieName);
+    movie.time = time;
+    movie.genre = genre;
+    movie.rating = rating;
 }
 
-void InitMovie(Movie& movie, const char* name, const double rating, const int duration, const Genre genre)
+void printName(const Movie& movie)
 {
-	StrCopy(movie.name, name);
-	movie.rating = rating;
-	movie.duration = duration;
-	movie.genre = genre;
+    int i = 0;
+    cout << "Name: ";
+    while(movie.movieName[i] != '\0')
+    {
+        cout << movie.movieName[i];
+        i++;
+
+    }
+    cout << "; ";
 }
 
-void PrintMovies(const Movie* moviesArr, const int count)
+void printGenre(const Movie& movie)
 {
-	for (size_t i = 0; i < count; i++) {
-		std::cout << "Movie name: " << moviesArr[i].name << std::endl;
-		std::cout << "Duration: " << moviesArr[i].duration << std::endl;
-		std::cout << "Rating: " << moviesArr[i].rating << std::endl;
-		std::cout << "Genre: ";
-		switch (moviesArr[i].genre) {
-			case Genre::Comedy: std::cout << "Comedy"; break;
-			case Genre::Action: std::cout << "Action"; break;
-			case Genre::Horror: std::cout << "Horror"; break;
-			case Genre::Romantic: std::cout << "Romantic"; break;
-		}
-		std::cout << std::endl;
-	}
+    if(movie.genre == Genre::Comedy)
+    {
+        cout << "Genre: Comedy; ";
+    }
+    else if(movie.genre == Genre::Drama)
+    {
+        cout << "Genre: Drama; ";
+    }
+    else if(movie.genre == Genre::Horror)
+    {
+        cout << "Genre: Horror; ";
+    }
+    else if(movie.genre == Genre::Thriller)
+    {
+        cout << "Genre: Thriller; ";
+    }
 }
 
-void SortMoviesByRating(Movie* movies, const int count)
+void printMovieArr(const Movie* movieArr, const size_t size)
 {
-	for (size_t i = 0; i < count - 1; ++i) {
-		size_t indexOfMaxElement = i;
-		for (size_t j = i + 1; j < count; ++j) {
-			if (movies[indexOfMaxElement].rating < movies[j].rating) {
-				indexOfMaxElement = j;
-			}
-		}
-		
-		if(indexOfMaxElement != i) {
-			Swap(movies[i], movies[indexOfMaxElement]);
-		}
-	}
-  
+    for(int i = 0; i<size; ++i)
+    {
+        cout << i+1 << ". ";
+        printName(movieArr[i]);
+        cout << "Duration: " << movieArr[i].time << "; ";
+        printGenre(movieArr[i]);
+        cout << "Rating: " << movieArr[i].rating << '\n';
+    }
 }
 
-void PrintNameOfBestRatedMovie(const Movie* movies, const int count)
+void sortMovieArr(Movie* movieArr, const size_t size)
 {
-	int indexOfBest = 0;
+    for(int i = 0; i<size; ++i)
+    {
+        for(int j = i+1; j<size; ++j)
+        {
+            if(movieArr[i].rating < movieArr[j].rating)
+            {
+                Movie temp = movieArr[i];
+                movieArr[i] = movieArr[j];
+                movieArr[j] = temp;
+            }
+        }
+    }
+}
 
-	for (size_t i = 1; i < count; i++) {
-		if (movies[indexOfBest].rating < movies[i].rating) {
-			indexOfBest = i;
-		}
-	}
-
-	std::cout << movies[indexOfBest].name << std::endl;
+void topMovie(Movie* movieArr, const size_t size)
+{
+    Movie top = movieArr[0];
+    for(int i = 1; i<size; ++i)
+    {
+        if(movieArr[i].rating > top.rating)
+        {
+            top = movieArr[i];
+        }
+    }
+    cout << "Top movie: \n";
+    printName(top);
+    cout << "Duration: " << top.time << "; ";
+    printGenre(top);
+    cout << "Rating: " << top.rating << '\n';
 }
 
 int main()
 {
-	int numberOfMovies;
-	std::cin >> numberOfMovies;
+    Movie myFirstMovie, mySecondMovie, myThirdMovie;
+    const char firstName[] = {'E', 's', 'c', 'a', 'p', 'e', ' ', 'r', 'o', 'o', 'm'};
+    const int firstTime = 123;
+    const Genre firstGenre = Genre::Horror;
+    const double firstRating = 4.8;
 
-	Movie* movies = new Movie[numberOfMovies];
+    const char secondName[] = {'L', 'o', 's', 't', ' ', 'c', 'i', 't', 'y'};
+    const int secondTime = 96;
+    const Genre secondGenre = Genre::Comedy;
+    const double secondRating = 5.2;
 
-	for (size_t i = 0; i < numberOfMovies; i++) {
-		char name[MAX_NAME_SIZE + 1];
-		std::cin >> name;
-		std::cin.ignore();
-		double rating;
-		int duration, type;
-		std::cin >> rating >> duration >> type;
-		Genre genre = (Genre)type; // TODO: validation / switch case
-		InitMovie(movies[i], name, rating, duration, genre);
-	}
+    const char thirdName[] = {'S', 'm', 'i', 'l', 'e'};
+    const int thirdTime = 102;
+    const Genre thirdGenre = Genre::Thriller;
+    const double thirdRating = 5.1;
 
-	PrintNameOfBestRatedMovie(movies, numberOfMovies);
-	SortMoviesByRating(movies, numberOfMovies);
-	PrintMovies(movies, numberOfMovies);
-  
-  	delete[] movies;
+    createMovie(myFirstMovie, firstName, firstTime, firstGenre, firstRating);
+    createMovie(mySecondMovie, secondName, secondTime, secondGenre, secondRating);
+    createMovie(myThirdMovie, thirdName, thirdTime, thirdGenre, thirdRating);
+    const size_t SIZE = 3;
+    Movie movieArr[SIZE] = {myFirstMovie, mySecondMovie, myThirdMovie};
+    printMovieArr(movieArr, SIZE);
+    topMovie(movieArr, SIZE);
+    cout << "Sorted:\n";
+    sortMovieArr(movieArr, SIZE);
+    printMovieArr(movieArr, SIZE);
+
 }

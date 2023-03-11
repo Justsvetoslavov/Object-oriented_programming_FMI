8aeb9fd467d0c675e7295e79eec7cc4f9e9171f3
NayleG <115502650+NayleG@users.noreply.github.com>
2023-03-11 14:40:34 +0200

    Task.08

--- Seminars/Sem.01/Pract.01/Task08.cpp
+++ Seminars/Sem.01/Pract.01/Task08.cpp
@@ -1,98 +1,113 @@
 #include <iostream>
+using std:: cout;
+using std:: cin;
 
-const int MAX_SIZE_NAME = 30;
-const int MAX_GROUP_SIZE = 30;
-
+const int MAX_SIZE = 30;
+const int MAX_NUM = 50;
 struct Student
 {
-	char name[MAX_SIZE_NAME + 1];
-	unsigned int fn;
-	double grade;
+    char studentName[MAX_SIZE];
+    double studentGrade;
 };
 
-struct StudentGroup
+struct Group
 {
-	size_t amountOfStudents;
-	double averageGrade;
-	Student groupArr[MAX_GROUP_SIZE];
+    char groupName[MAX_SIZE];
+    int numberOfStudents;
+    Student membersOfTheGroup[MAX_NUM];
+    double avrGrade;
 };
 
-double FindSum(const Student* arr, int numberOfStudents)
+void addStudent(Student& member)
 {
-	double sum = 0;
-	for (size_t i = 0; i < numberOfStudents; i++) {
-		sum += arr[i].grade;
-	}
-
-	return sum;
+    cout << "Student name: ";
+    cin >> member.studentName;
+    cin.ignore();
+    cout << "Student grade: ";
+    cin >> member.studentGrade;
+    cin.ignore();
 }
 
-void InitGroupOfStudents(StudentGroup& group)
+void newGroup(Group& group)
 {
-	for (size_t i = 0; i < group.amountOfStudents; i++) {
-		std::cin >> group.groupArr[i].fn;
-		std::cin >> group.groupArr[i].name;
-		std::cin.ignore();
-		std::cin >> group.groupArr[i].grade;
-	}
+    char* name = new char[MAX_SIZE];
+    cout << "Group name: ";
+    cin.getline(group.groupName, MAX_SIZE);
+    cout << "The number of students in the group is: ";
+    cin >> group.numberOfStudents;
+    double sum=0;
+    int br = 0;
+    for(int i = 0; i<group.numberOfStudents; ++i)
+    {
+        Student student;
+        addStudent(student); 
+        group.membersOfTheGroup[i] = student;
+        sum+=student.studentGrade;
+        ++br;
+    }
 
-	group.averageGrade = FindSum(group.groupArr, group.amountOfStudents) / group.amountOfStudents;
+    group.avrGrade = sum/br;
 }
 
-size_T NumberOfStudentsTakingScholarship(const StudentGroup& group, const double minGrade) {
-	size_t studentCounter = 0;
-
-	for (size_t i = 0; i < group.amountOfStudents; i++) {
-		if (group.groupArr[i].grade >= minGrade) {
-			studentCounter++;
-		}
-	}
-
-	return studentCounter;
-}
-
-void Swap(Student& a, Student& b) {
-	Student temp = a;
-	a = b;
-	b = temp;
-}
-
-void SortStudentsByGrade(Student* students, const int size) {
-	for (size_t i = 0; i < size - 1; i++) {
-		size_t minIndex = i;
-		for (size_t j = i + 1; j < size; j++) {
-			if (students[j].grade < students[minIndex].grade) {
-				minIndex = j;
-			}
-		}
-
-		if (minIndex != i) {
-			Swap(students[minIndex], students[i]);
-		}
-	}
-
-}
-
-void PrintStudent(const Student& student) {
-	std::cout << "Faculty number: " << student.FN << ", Name: " << student.name <<
-		", Grade: " << student.grade << std::endl;
-}
-
-void StudentsTakingScholarship(StudentGroup& group, const double minGrade)
+int numberScholarship(const Group& group, const double& minGrade)
 {
-	size_T numberOfStudents = NumberOfStudentsTakingScholarship(group, minGrade);
-	SortStudentsByGrade(group.groupArr, group.amountOfStudents);
-	
-	for (size_t i = group.amountOfStudents - 1; i >= group.amountOfStudents - numberOfStudents; i--){
-		PrintStudent(group.groupArr[i]);
-	}
+    int count = 0;
+    for(int i = 0; i<group.numberOfStudents; i++)
+    {
+        if(group.membersOfTheGroup[i].studentGrade >= minGrade)
+        {
+            ++count;
+        }
+    }
+
+    return count;
+}
+
+void sortScholarship(Student* membersOfTheGroup, const int& size)
+{
+    for(int i = 0; i<size-1; ++i)
+    {
+        for(int j = i+1; j<size; ++j)
+        {
+            if(membersOfTheGroup[i].studentGrade - membersOfTheGroup[j].studentGrade < 0)
+            {
+                Student temp = membersOfTheGroup[i];
+                membersOfTheGroup[i] = membersOfTheGroup[j];
+                membersOfTheGroup[j] = temp;
+            }
+        }
+    }
+}
+
+void printScholarship(const Group& group, const double& minGrade)
+{
+    Student scholarShip[numberScholarship(group, minGrade)];
+    for(int i = 0, j= 0; i<group.numberOfStudents; ++i)
+    {
+        if(group.membersOfTheGroup[i].studentGrade >= minGrade)
+        {
+            scholarShip[j] = group.membersOfTheGroup[i];
+            ++j;
+        }
+    }
+
+    sortScholarship(scholarShip,numberScholarship(group, minGrade));
+
+    if(numberScholarship(group, minGrade)>0)
+    {
+        cout << "Top students: \n";
+        for(int i = 0; i< numberScholarship(group, minGrade); ++i)
+        {
+            cout << i+1 << ". Name: " << scholarShip[i].studentName << " -> Grade: " << scholarShip[i].studentGrade << '\n';
+        }
+    }
 }
 
 int main()
 {
-	StudentGroup group;
-	std::cin >> group.amountOfStudents;
-	
-	initGroupOfStudents(group);
-	studentsTakingScholarship(group, 5);
+    Group myGroup;
+    newGroup(myGroup);
+    const double minGrade = 4.70;
+    cout << "Number of students with scholarship: " << numberScholarship(myGroup, minGrade) << '\n';
+    printScholarship(myGroup, minGrade);
 }

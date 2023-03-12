4b91e73112249c4ea468a6b511123137e5cfc7f8
N1k1ta-white <65460812+N1k1ta-white@users.noreply.github.com>
2023-03-12 14:15:03 +0200

    Update Task04.cpp

--- Task04.cpp
+++ Task04.cpp
@@ -13,12 +13,12 @@ struct Relation {
     Pair pairs[25];
 };
 
-Pair createPair(const int &a = 0, const int &b = 0) {
+Pair createPair(const int a = 0, const int b = 0) {
     Pair res{a, b};
     return res;
 }
 
-void initPair(Pair &pair, const int &a, const int &b) {
+void initPair(Pair &pair, const int a, const int b) {
     pair.a = a;
     pair.b = b;
 }
@@ -27,29 +27,14 @@ void writePairToFile(std::fstream &file, const Pair &pair) {
     file << pair.a << " " << pair.b << " ";
 }
 
-Pair readPairFromFile(std::fstream &file, const int &mode = 0) {
+Pair readPairFromFile(std::fstream &file, const int mode = 0) {
     size_t curr_pos;
     if (mode == 0) {
         curr_pos = file.tellg();
         file.seekg(std::ios::beg);
     }
     Pair res;
-    char rotator;
-    int number = 0, time = 1;
-    while (file.get(rotator)) {
-        if (rotator == ' ') {
-            if (time == 1)
-                res.a = number;
-            else {
-                res.b = number;
-                break;
-            }
-            number = 0;
-            time++;
-            continue;
-        }
-        number = number * 10 + (int) rotator - 48;
-    }
+    file >> res.a >> res.b;
     if (mode == 0) {
         file.clear();
         file.seekg(curr_pos);

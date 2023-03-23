45edae378142a12c8a34d48574b40c76d4f751c4
MartinM2304 <m.marinkov6@mail.bg>
2023-03-23 18:46:50 +0200

    task01 done

--- Seminars/Sem.04/Pract.04/task01.cpp
+++ Seminars/Sem.04/Pract.04/task01.cpp
@@ -1,3 +1,42 @@
-//
-// Created by AZ on 16.03.2023 г..
-//
+#include <iostream>
+
+char* copyString(char *arr,char* from){
+
+    for(int i=0;from[i]!='\0';i++){
+        arr[i]=from[i];
+    }
+    return arr;
+}
+
+class Teacher{
+    char name[30];
+    int age;
+    int internship;
+public:
+
+    Teacher(char* name, int age, int inter){
+        setName(name);
+        setAge(age);
+        setInter(inter);
+    }
+    void setName( char*name){
+        copyString(this->name,name);
+    }
+    char* getName(char*name){
+        copyString(name,this->name);
+    }
+
+    void setAge(int age){
+        this->age=age;
+    }
+    int getAge(){
+        return age;
+    }
+
+    void setInter(int inter){
+        this->internship=inter;
+    }
+    int getInter(){
+        return internship;
+    }
+};

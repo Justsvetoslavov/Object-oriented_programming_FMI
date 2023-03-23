9eb2b986a164642233478b390e8ce6e94be37011
Yonko Draganov <draganovyonko@gmail.com>
2023-03-23 19:33:39 +0200

    Add Task04

---
+++ Seminars/Sem.04/Pract.04/Task04/Secret.h
@@ -0,0 +1,17 @@
+#pragma once
+const unsigned short MAX_DESCRIPTION_LENGTH = 1024;
+const unsigned short MAX_PASSWORD_LENGTH = 64;
+
+class Secret
+{
+	char _taskDescription[MAX_DESCRIPTION_LENGTH];
+	char _password[MAX_PASSWORD_LENGTH];
+	mutable size_t _failedLogins;
+public:
+	Secret(const char* taskDescription, const char* password);
+
+	const char* getTask(const char* pwd) const;
+	void setPassword(const char* newPassword, const char* oldPassword);
+	void setTask(const char* newTask, const char* passwd);
+	size_t getLoginFails() const;
+};

056b9e90bad2c737aa6944af263068ddcca16a51
plamenstoynev <79408379+plamenstoynev@users.noreply.github.com>
2023-04-03 22:53:17 +0300

    Create ComplexNum.hpp

---
+++ Seminars/Sem.05/Pract. 05/Task02/ComplexNum.hpp
@@ -0,0 +1,37 @@
+#pragma once
+class ComplexNum
+{
+private:
+	double a;
+	double b;
+public:
+	ComplexNum() = default;
+
+	ComplexNum(double a, double b);
+
+	ComplexNum(const ComplexNum& other) = default;
+
+	void setA(double a);
+
+	void setB(double b);
+
+	double getA() const;
+
+	double getB() const;
+
+	bool isEqual(const ComplexNum& cocomplexNum1, const ComplexNum& complexNum2);
+
+	ComplexNum add(const ComplexNum& cocomplexNum1, const ComplexNum& complexNum2);
+
+	ComplexNum substract(const ComplexNum& complexNum1, const ComplexNum& complexNum2);
+
+	ComplexNum multiply(const ComplexNum& cocomplexNum1, const ComplexNum& complexNum2);
+	
+	ComplexNum conjugate(ComplexNum& complexNum);
+
+	ComplexNum getConjugated(ComplexNum& complexNum);
+
+	void print(const ComplexNum& complexNum);
+
+	~ComplexNum() = default;
+};

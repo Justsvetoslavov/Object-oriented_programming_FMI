fa9fb06d6f9d88ea51ca0e4bd61e0c99a35c61d3
Nikola Svetoslavov <justsvetoslavov@gmail.com>
2023-04-05 01:21:34 +0300

    Add EventCollection Solution

---
+++ Seminars/Sem.05/EventCollection/EventCollection.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "../Event/Event.h"
+
+class EventCollection {
+private:
+	Event data[20];
+	unsigned count = 0; // 0 ... count - 1 са добавени. От count до 19 са обекти създадени с default-на стойност,
+public:
+	EventCollection() = default;
+	bool addEvent(const Event& event);
+	void remove(unsigned ind);
+	const Event& getByIndex(unsigned ind) const;
+};

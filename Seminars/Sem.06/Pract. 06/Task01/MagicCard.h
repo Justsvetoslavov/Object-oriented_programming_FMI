0e390d4d59f35fa237f662dfdb15bac7049e5c21
gabrielavm <115610325+gabrielavm@users.noreply.github.com>
2023-05-02 00:19:38 +0300

    Update MagicCard.h

--- Seminars/Sem.06/Pract. 06/Task01/MagicCard.h
+++ Seminars/Sem.06/Pract. 06/Task01/MagicCard.h
@@ -1,29 +1,39 @@
 #pragma once
+#include <iostream>
+#pragma warning(disable : 4996)
 
-const short NAME_MAX_SIZE = 26;
-const short EFFECT_MAX_SIZE = 101;
+const int MAX_NAME_SIZE = 25;
+const int MAX_EFFECT_SIZE = 100;
 
-enum class MagicCardType {
-	trap,
-	buff,
-	spell,
-	unknown
+enum class CardType
+{
+    trap,
+    buff,
+    spell,
+    unknown
 };
 
-class MagicCard {
-private:
-	char name[NAME_MAX_SIZE];
-	char effect[EFFECT_MAX_SIZE];
-	MagicCardType type;
+class MagicCard
+{
+    char name[MAX_NAME_SIZE + 1];
+    char effect[MAX_EFFECT_SIZE + 1];
+    CardType type;
+
 public:
-	MagicCard();
-	MagicCard(const char* name, const char* effect, const MagicCardType type);
 
-	MagicCard(const MagicCard& magicCard) = default;
-	MagicCard& operator=(const MagicCard& magicCard) = default;
-	~MagicCard() = default;
+    MagicCard();
 
-	const char* const getName() const;
-	const char* const getEffect() const;
-	MagicCardType getMagicCardType() const;
+    MagicCard(const char* name, const char* effect, const CardType type);
+
+    MagicCard(const MagicCard& other);
+
+    MagicCard& operator=(const MagicCard& other);
+
+    ~MagicCard();
+
+    const char* getName() const;
+
+    const char* getEffect() const;
+
+    CardType getType() const;
 };

72ec837b0b00d7e23284e505cef8b5f9cf4efc8a
plamenstoynev <79408379+plamenstoynev@users.noreply.github.com>
2023-05-06 21:00:46 +0300

    Create String.hpp

---
+++ Seminars/Sem.10/Pract.10/String.hpp
@@ -0,0 +1,55 @@
+#pragma once
+#include <ostream>
+
+
+class String
+{
+private:
+    char* myStr;
+public:
+    String() = default;
+
+    String(char* string);
+
+    String(const String& other);
+
+    String& operator=(const String& other);
+
+    char operator[](size_t index);
+
+    String& operator+=(const String& other);
+
+    friend String operator+(const String& lStr, const String& rStr);
+
+    friend bool operator==(const String& lStr, const String& rStr);
+
+    friend bool operator!=(const String& lStr, const String& rStr);
+
+    friend bool operator>(const String& lStr, const String& rStr);
+
+    friend bool operator<(const String& lStr, const String& rStr);
+
+    friend bool operator>=(const String& lStr, const String& rStr);
+
+    friend bool operator<=(const String& lStr, const String& rStr);
+
+    friend std::ostream& operator<<(std::ostream& os, const String& string);
+
+    friend std::istream& operator>>(std::istream& is, const String& string);
+
+    void setMyStr(const char* string);
+
+    char* getMyStr() const;
+
+    friend size_t length(const String& myStr);
+
+    bool isEmpthy();
+
+    void clear(String& myStr);
+
+    const char* c_str(const String& myStr);
+
+    String append(char letter);
+
+    ~String();
+};

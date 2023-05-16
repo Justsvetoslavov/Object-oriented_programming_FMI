3967f80de5641680004dd4880743efe066a4f931
Nikola Svetoslavov <justsvetoslavov@gmail.com>
2023-05-17 02:04:07 +0300

    Add Shape hierarchy Solution

---
+++ Seminars/Sem.11/Solutions/Shapes/cpp/Circle.cpp
@@ -0,0 +1,19 @@
+#include "Circle.h"
+
+const double PI = 3.1415;
+
+Circle::Circle(int x, int y, double radius) : Shape(1), radius(radius) {
+	setPoint(0, x, y);
+}
+
+double Circle::GetArea() const {
+	return PI * radius * radius;
+}
+
+double Circle::GetPer() const {
+	return 2 * PI * radius;
+}
+
+bool Circle::IsPointIn(int x, int y) const {
+	return Point(x, y).getDist(getPointAtIndex(0)) <= radius;
+}
---
+++ Seminars/Sem.11/Solutions/Shapes/cpp/main.cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+
+#include "Rectangle.h"
+#include "Circle.h"
+#include "Triangle.h"
+
+
+void PrintAreas(const Shape* const * shapes, size_t shapesCount) {
+	for (size_t i = 0; i < shapesCount; ++i) {
+		std::cout << shapes[i]->getArea() << std::endl;
+	}
+}
+
+void PrintPers(const Shape* const * shapes, size_t shapesCount) {
+	for (size_t i = 0; i < shapesCount; ++i) {
+		std::cout << shapes[i]->getPer() << std::endl;
+	}
+}
+
+void CheckPointIn(const Shape* const* shapes, size_t shapesCount, int x, int y) {
+	for (size_t i = 0; i < shapesCount; ++i) {
+		std::cout << shapes[i]->isPointIn(x, y) << std::endl;
+	}
+}
+
+void FreeCollection(Shape** shapes, size_t shapesCount) {
+	for (size_t i = 0; i < shapesCount; ++i) {
+			delete shapes[i];
+	}
+
+	delete[] shapes;
+}
+
+int main() {
+		Shape** arr = new Shape*[4];
+
+		arr[0] = new Rectangle(3, 4, 6, 8);
+		arr[1] = new Circle(3, 3, 4);
+		arr[2] = new Circle(1, 4, 5);
+		arr[3] = new Triangle(1, 1, 2, 2, 3, 3);
+
+		PrintAreas(arr, 4);
+		std::cout << "-----SEPARATOR----\n";
+		PrintPers(arr, 4);
+		std::cout << "-----SEPARATOR----\n";
+		CheckPointIn(arr, 4, 3, 3);
+		
+		FreeCollection(arr, 4);
+}
